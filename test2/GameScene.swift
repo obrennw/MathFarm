423fdc271218f1f26d38383752473ca148a5e949
Oscar Brennwald <obrennw@gmail.com>
2018-03-04 17:34:40 -0500

    documentation changes, integrated John's branch, etc

--- test2/GameScene.swift
+++ test2/GameScene.swift
@@ -16,20 +16,21 @@ struct ColliderType{
     static let Bucket:UInt32 = 2
 }
 
-/// Describes whether object is movable or static
-struct MoveType {
-    static let Movable:UInt32 = 1
-    static let Static:UInt32 = 2
-}
 
-/// A list of any objects that have static MoveType
+/// A list of static objects
 private let staticImages = ["bucket2"]
-private let Speaker = AVSpeechSynthesizer() //John
+/// A list of movable objects
+private let movableImages = ["apple"]
+
+private let moveTypeKey = "moveType"
+private let Speaker = AVSpeechSynthesizer()
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     let scoreText = SKLabelNode(fontNamed: "Arial")
     let background = SKSpriteNode(imageNamed: "blue-shooting-stars")
     var score = 0
+    
+    //The sprite currently being touched (if any)
     var selectedNode = SKSpriteNode()
     
     required init?(coder aDecorder: NSCoder){
@@ -60,10 +61,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         for i in 0..<imageNames.count {
                 let imageName = imageNames[i]
-                
+
                 let sprite = SKSpriteNode(imageNamed: imageName)
-                sprite.name = imageName
                 sprite.isAccessibilityElement = true
+                sprite.name = imageName
                 sprite.physicsBody = SKPhysicsBody(circleOfRadius: max(sprite.size.width / 2,
                                                                        sprite.size.height / 2))
                 sprite.physicsBody?.affectedByGravity = false
@@ -79,7 +80,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     sprite.position = CGPoint(x: size.width * offsetFraction, y: (size.height/(1.25))-(1.5*(sprite.size.height)*CGFloat(i)))
                 }
                 else{
-                    sprite.name = staticName
                     sprite.accessibilityLabel = "bucket"
                     sprite.physicsBody?.categoryBitMask = ColliderType.Bucket
                     sprite.physicsBody?.collisionBitMask = 0
@@ -96,14 +96,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(scoreText)
     }
     
+    /// Called when screen is touched
+    ///
+    /// - Parameters:
+    ///   - touches: Set of touches submitted by event
+    ///   - event: UIEvent triggering the fucntion
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for touch in touches {
             let positionInScene = touch.location(in: self)
-            selectNodeForTouch(touchLocation: positionInScene)
+            let touchedNode = self.atPoint(_:positionInScene)
+            if (touchedNode is SKSpriteNode){
+                onSpriteTouch(touchedNode: touchedNode as! SKSpriteNode)
+            }
         }
     }
     
+    /// Called when contact between two objects is initiated
+    ///
+    /// - Parameter contact: The object that refers to the contact caused by the two objects
     func didBegin(_ contact: SKPhysicsContact) {
         if (contact.bodyA.categoryBitMask == ColliderType.Object && contact.bodyB.categoryBitMask == ColliderType.Bucket) {
             score += 1
@@ -114,50 +125,52 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    /// Converts degrees to radians
+    ///
+    /// - Parameter degree: value in degrees
+    /// - Returns: converted value in radians
     func degToRad(degree: Double) -> CGFloat {
         return CGFloat(Double(degree) / 180.0 * Double.pi)
     }
     
-    func selectNodeForTouch(touchLocation: CGPoint) {
-        // 1
-        let touchedNode = self.atPoint(_:touchLocation)
-        if touchedNode is SKSpriteNode {
-            // 2
-            if !selectedNode.isEqual(touchedNode) {
-                selectedNode.removeAllActions()
-                selectedNode.run(SKAction.rotate(toAngle: 0.0, duration: 0.1))
-                
-                selectedNode = touchedNode as! SKSpriteNode
-                
-                // 3
-                if touchedNode.name! == movableName {
-                    let sequence = SKAction.sequence([SKAction.rotate(byAngle: degToRad(degree: -5.0), duration: 0.1),
-                                                      SKAction.rotate(byAngle: 0.0, duration: 0.1),
-                                                      SKAction.rotate(byAngle: degToRad(degree: 5.0), duration: 0.1)])
-                    selectedNode.run(SKAction.repeatForever(sequence))
-                }
+    /// Clears out wobble action from selected node and makes touched node wobble
+    ///
+    /// - Parameter touchedNode: sprite being touched
+    func onSpriteTouch(touchedNode: SKSpriteNode) {
+        selectedNode.run(SKAction.rotate(toAngle: 0.0, duration: 0.1))
+        selectedNode.removeAllActions()
+        selectedNode = touchedNode
+        if movableImages.contains(touchedNode.name!) && !touchedNode.hasActions() {
+            let sequence = SKAction.sequence([SKAction.rotate(toAngle: degToRad(degree: -2.0), duration: 0.1),
+                                              SKAction.rotate(toAngle: 0.0, duration: 0.1),
+                                              SKAction.rotate(toAngle: degToRad(degree: 2.0), duration: 0.1)])
+            selectedNode.run(SKAction.repeatForever(sequence))
+        }
+    }
+    
+    /// Handles single finger drag on device
+    ///
+    /// - Parameters:
+    ///   - touches: Set of touches that caused event
+    ///   - event: UIEvent that triggered function
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            //Drag sprite to new position if it is being touched
+            let nodePosition = selectedNode.position
+            let currentPosition = touch.location(in: self)
+            let previousPosition = touch.previousLocation(in: self)
+            let translation = CGPoint(x: currentPosition.x - previousPosition.x, y: currentPosition.y - previousPosition.y)
+            if  ((self.atPoint(currentPosition).isEqual(selectedNode) || self.atPoint(previousPosition).isEqual(selectedNode)
+                ) && movableImages.contains(selectedNode.name!)){
+                selectedNode.position = CGPoint(x: nodePosition.x + translation.x, y: nodePosition.y + translation.y)
             }
         }
     }
     
-    func panForTranslation(translation: CGPoint) {
-        let position = selectedNode.position
-
-        if selectedNode.name! == movableName {
-            selectedNode.position = CGPoint(x: position.x + translation.x, y: position.y + translation.y)
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let positionInScene = touch.location(in: self)
-            let previousPosition = touch.previousLocation(in: self)
-            let translation = CGPoint(x: positionInScene.x - previousPosition.x, y: positionInScene.y - previousPosition.y)
-            panForTranslation(translation: translation)
-        }
-    }
-    
-    func SpeakString(text: String) { //JOHN
+    /// Prompts text to be spoken out by device
+    ///
+    /// - Parameter text: text to be spoken
+    func SpeakString(text: String) {
             let Utterance = AVSpeechUtterance(string: text)
             Speaker.speak(Utterance)
     }
