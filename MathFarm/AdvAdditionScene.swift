7a5c266f77adda4f8a187bc88fe4928d9aca6238
Mian Xing <mianx@macpro-stotts.cs.unc.edu>
2018-04-19 20:15:44 -0400

    added sub level selection controller to select addition game difficulty. Also initialize adv addition scene

--- MathFarm/AdvAdditionScene.swift
+++ MathFarm/AdvAdditionScene.swift
@@ -17,7 +17,7 @@ class AdvAdditionScene: SKScene, SKPhysicsContactDelegate {
     var contactFlag = false
     var selectedNode = SKSpriteNode()
     var winningStreak: Int?
-    var nodeOriginalPosition: CGPoint?
+    var backButton = SKSpriteNode(imageNamed: "turtle")
     var answer = 0
     
     let rightObjectType = movableImages[Int(arc4random_uniform(UInt32(movableImages.count)))]
@@ -32,16 +32,46 @@ class AdvAdditionScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         
-        let submitButtonText = SKLabelNode(fontNamed: "Arial")
-        submitButtonText.text = "submit"
-        submitButtonText.fontColor = .blue
-        let submitButton = SKSpriteNode()
-        submitButton.isAccessibilityElement = true
-        submitButton.accessibilityLabel = "take the basket to farmer Joe"
-        submitButton.color = .red
-        submitButton.addChild(submitButtonText)
-        self.addChild(submitButton)
+        let backgroundNode = SKSpriteNode(imageNamed: "woodenBackground")
+        backgroundNode.name = "backGround"
+        backgroundNode.size = CGSize(width: size.width, height: size.height)
+        backgroundNode.position = CGPoint(x: size.width/2, y: size.height/2)
+        backgroundNode.zPosition = -3
+        self.addChild(backgroundNode)
+        
+        backButton.position = CGPoint(x: size.width * 0.1, y: size.height * 0.9)
+        backButton.name = "back to level selection"
+        backButton.isAccessibilityElement = true
+        backButton.accessibilityLabel = "go back and start a new farm task"
+        self.addChild(backButton)
+
+        
+//        let submitButtonText = SKLabelNode(fontNamed: "Arial")
+//        submitButtonText.text = "submit"
+//        submitButtonText.fontColor = .blue
+//        let submitButton = SKSpriteNode()
+//        submitButton.isAccessibilityElement = true
+//        submitButton.accessibilityLabel = "take the basket to farmer Joe"
+//        submitButton.color = .red
+//        submitButton.addChild(submitButtonText)
+//        self.addChild(submitButton)
         
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let positionInScene = touch.location(in:self)
+            let touchedNode = self.atPoint(_:positionInScene)
+            
+            if(touchedNode is SKSpriteNode) {
+                if(touchedNode.name == "back to level selection") {
+                    self.removeAllActions()
+                    self.removeAllChildren()
+                    self.game_delegate?.backToLevel()
+                }
+            }
+        }
+    }
+
 
 }
