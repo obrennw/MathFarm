28b4d536e955ab67f6eb426fc75ae88af97ea3fa
Oscar Brennwald <obrennw@gmail.com>
2018-05-01 19:14:07 -0400

    several changes

--- MathFarm/LevelSelectController.swift
+++ MathFarm/LevelSelectController.swift
@@ -12,13 +12,14 @@ import SpriteKit
 /// ViewController for level selector
 class LevelSelectController: UIViewController {
 
-    /// Button that links to couting level
-    @IBOutlet weak var CountingBtn: UIButton!
+
+    @IBOutlet weak var CountingEasy: UIButton!
     /// Button that links to addition level
     @IBOutlet weak var AdditionBtn: UIButton!
     //button that links to pattern level
     @IBOutlet weak var PatternBtn: UIButton!
     
+    @IBOutlet weak var CountingPro: UIButton!
     /// Do any additional setup after loading the view.
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,18 +33,24 @@ class LevelSelectController: UIViewController {
     /// Transitions to GameViewController to present counting level
     ///
     /// - Parameter sender: Component that triggers function on action
-    @IBAction func toCountingLevel(_ sender: UIButton) {
-        performSegue(withIdentifier: "toCounting", sender: self)
+
+    @IBAction func toCountingEasy(_ sender: UIButton) {
+        performSegue(withIdentifier: "toCountingEasy", sender: self)
     }
-    
+    @IBAction func toCountingPro(_ sender: UIButton) {
+        performSegue(withIdentifier: "toCountingPro", sender: self)
+    }
     @IBAction func toAdditionLevel(_ sender: UIButton) {
         performSegue(withIdentifier: "toSubLvlSelection", sender: self)
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if(segue.identifier=="toCounting") {
+        if(segue.identifier == "toCountingEasy") {
             let vc = segue.destination as! GameViewController
-            vc.gameType = "counting"
+            vc.gameType = "countingEasy"
+        } else if (segue.identifier=="toCountingPro"){
+            let vc = segue.destination as! GameViewController
+            vc.gameType = "countingPro"
         }
         if(segue.identifier=="toSubLvlSelection") {
             let vc = segue.destination as! SubLevelSelectController
